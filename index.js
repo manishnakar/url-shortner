80a312133cda38d1a9cb3776d1967cbc90edf6ce
Manish Ravilal Nakar <manish.nakar@LT-31143.local>
2021-04-03 14:37:37 +0530

    created all routes

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json({extended: false}));
 
 
 app.get('/', (req, res)=> {
 
     res.send('hello world ')
+})
+
+app.get('/api/:shortUrl', (req, res) => {
+    res.send(' redirect to full url ')
+})
+
+app.post('/api/url', (req, res) => {
+
+    res.json({ message: 'short url created '});
 })
 
 
