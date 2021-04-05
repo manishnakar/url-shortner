237be23c2a90da5328123fd9f7d10a043d10ae9f
Manish Ravilal Nakar <manish.nakar@LT-31143.local>
2021-04-05 16:17:04 +0530

    fixed eslint setup error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,20 @@ module.exports = {
   extends: 'standard',
   env: {
     node: true,
-    mocha: true
+    es6: true
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-useless-escape': 'off',
+    eqeqeq: ['error', 'smart'],
+    'no-console': 'error',
+    camelcase: ['error'],
+    'no-extra-bind': ['error'],
+    'no-multi-spaces': ['error'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-var': 'error',
+    'comma-spacing': ['error', { before: false, after: true }]
   }
 }
